08d46d248d3b30a3944488432c12f87a57b5fba1
Todd V. Rovito <rovitotv@gmail.com>
2015-02-24 22:36:51 -0500

    -Now the script works....spelling smtplib right helps alot!

--- tests/sendSpam.py
+++ tests/sendSpam.py
@@ -2,15 +2,18 @@
 # this test sends a SpamTest email message that will definitely be marked
 # as spam.  There is a neat trigger called GTUBE (generic trigger for
 # unsolicited bulk Email)
-import smptlib
+import smtplib
 
 from email.mime.text import MIMEText
 
-msg = "this is a test email that will trigger spam\n"
-msg += "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X"
+sendStr = "this is a test email that will trigger spam\n"
+sendStr += "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X"
+msg = MIMEText(sendStr)
 msg['Subject'] = 'Spam trigger for sure'
-me = 'rovitotv@rovitotv.org'
-you = 'spamking@randomdomain.com'
+# me == the sender's email address
+# you == the recipient's email address
+me = 'spamkind@randomdomain.com'
+you = 'rovitotv@rovitotv.org'
 msg['From'] = you
 msg['To'] = me
 
